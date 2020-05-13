cfbd452a18bbe846d7341dbf42c39232f8e4c092
sv <sv@P11M>
2020-05-13 08:44:03 +0700

    Thuchanh1

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<style>
+.error {color:#FF0000;}
+</style>
+</head>
+<body>
+<h2> PHP Form Validation Example </h2>
+<form method="post" action="">
+Username : <input type="text" name="username" value""><br>
+Password : <input type="text" name="password" value""><br>
+<input type="submit" name="submit" value="submit">
+</form>
